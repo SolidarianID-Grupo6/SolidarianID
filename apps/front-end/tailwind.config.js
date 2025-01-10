92e6b7d0dbd520643dea0170c42e2a1607c5998a
Raúl Hernández Martínez <raul.hernandezm@um.es>
2025-01-10 23:17:59 +0100

    Adaptación de pantallas

--- apps/front-end/tailwind.config.js
+++ apps/front-end/tailwind.config.js
@@ -1,15 +1,7 @@
 module.exports = {
-  safelist: [
-    'bg-red-500',
-    'hover:bg-red-600',
-    'text-gray-800',
-    'text-teal-500',
-    'hover:text-teal-300',
-  ],
   content: [
     './src/**/*.{html,js,ts,jsx,tsx,hbs}',
     './views/**/*.hbs',
-    './public/**/*.html'
   ],
   theme: {
     extend: {},
