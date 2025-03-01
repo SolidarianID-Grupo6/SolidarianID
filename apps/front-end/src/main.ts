e0645259eb37506e6b2e526bc7dda92edceca89f
Raúl Hernández Martínez <raulhernandez.rlhnmn@gmail.com>
2025-03-02 00:33:21 +0100

    Fixed MVC Architecture
    Co-Authored-By: Raúl Hernández Martínez <91872253+raul-umu@users.noreply.github.com>

--- apps/front-end/src/main.ts
+++ apps/front-end/src/main.ts
@@ -1,18 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import * as exphbs from 'express-handlebars';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  // Configura la ruta absoluta para vistas y activos estáticos
-  app.setBaseViewsDir('/app/apps/front-end/src/views');
+  // Configura express-handlebars con opciones personalizadas, incluyendo partials
+  app.engine('hbs', exphbs.engine({
+    extname: '.hbs',
+    defaultLayout: 'layout', // Asegúrate de tener un layout (por ejemplo, layout.hbs)
+    layoutsDir: ('/app/apps/front-end/src/views/layouts'),
+    partialsDir: ('/app/apps/front-end/src/views/partials'),
+  }));
+
+  // Configura el motor de vistas y las carpetas de vistas y activos estáticos
   app.setViewEngine('hbs');
+  app.setBaseViewsDir('/app/apps/front-end/src/views');
   app.useStaticAssets('/app/apps/front-end/src/public');
 
-  console.log('Views Directory:', '/app/apps/front-end/src/views');
-  console.log('Static Assets Directory:', '/app/apps/front-end/src/public');
-  
+  console.log('Views Directory:', ('/app/apps/front-end/src/views'));
+  console.log('Static Assets Directory:', ('/app/apps/front-end/src/public'));
 
   await app.listen(process.env.PORT ?? 3004);
 }
